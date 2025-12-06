1343bfafb8652ef829b9e7b61052dd2938869879
TanveerKalluru <146471689+TanveerKalluru@users.noreply.github.com>
2025-12-06 19:34:53 +0530

    Update app.py

--- fashion-recommendation-System-main/app.py
+++ fashion-recommendation-System-main/app.py
@@ -18,12 +18,7 @@ model = tensorflow.keras.Sequential([
 # print(model.summary())
 
 def feature_extraction(img_path,model):
-    img = image.load_img(img_path, target_size=(224, 224))
-    img_array = image.img_to_array(img)
-    expanded_img_array = np.expand_dims(img_array, axis=0)
-    preprocessed_img = preprocess_input(expanded_img_array)
-    result = model.predict(preprocessed_img).flatten()
-    normalized_result = result / norm(result)
+    
 
     return normalized_result
 
